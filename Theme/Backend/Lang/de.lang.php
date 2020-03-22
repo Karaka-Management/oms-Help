fb1fa5582078af4855ab8ff81d31ac8643707b70
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-22 18:42:34 +0100

    implement localization

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Help
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Help' => [
+    'Module'  => 'Modul',
+    'Modules' => 'Module',
+    'Name'    => 'Name',
+]];

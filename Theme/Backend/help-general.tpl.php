8a2a199a8323083a477ac00fcd31febfff1a3d06
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-06-25 22:06:55 +0200

    use portlets

--- Theme/Backend/help-general.tpl.php
+++ Theme/Backend/help-general.tpl.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xs-12 col-md-8 col-lg-9">
-        <section class="box wf-100">
+        <section class="portlet">
             <article>
                 <?= $this->getData('content'); ?>
             </article>
@@ -8,7 +8,7 @@
     </div>
 
     <div class="col-xs-12 col-md-4 col-lg-3">
-        <section class="box wf-100">
+        <section class="portlet">
             <article>
                 <?= $this->getData('navigation'); ?>
             </article>
--- Theme/Backend/help-module.tpl.php
+++ Theme/Backend/help-module.tpl.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xs-12 col-md-8 col-lg-9">
-        <section class="box wf-100">
+        <section class="portlet">
             <article>
                 <?= $this->getData('content'); ?>
             </article>
@@ -12,14 +12,14 @@
             <a tabindex="0" class="button" href="<?= \phpOMS\Uri\UriFactory::build('{/lang}/backend/help/module/single?id={?id}'); ?>"><?= $this->getHtml('Module'); ?></a>
         </div>
 
-        <section class="box wf-100">
+        <section class="portlet">
             <article>
                 <?= $this->getData('navigation'); ?>
             </article>
         </section>
 
         <?php if ($this->hasData('devNavigation')) : ?>
-        <section class="box wf-100">
+        <section class="portlet">
             <article>
                 <?= $this->getData('devNavigation'); ?>
             </article>

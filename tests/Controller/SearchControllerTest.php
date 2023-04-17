d68759a286812d6a1be697f1bf2281b6bdb20590
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-04-17 19:44:57 +0200

    test fixes

--- tests/Controller/SearchControllerTest.php
+++ tests/Controller/SearchControllerTest.php
@@ -68,7 +68,7 @@ final class SearchControllerTest extends \PHPUnit\Framework\TestCase
 
         $permission = new AccountPermission();
         $permission->setUnit(1);
-        $permission->setApp('api');
+        $permission->setApp(1);
         $permission->setPermission(
             PermissionType::READ
             | PermissionType::CREATE

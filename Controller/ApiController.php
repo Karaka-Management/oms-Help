378f6109b452408390a94fb3be3943d2e36dabc0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:11 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Help
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Help\Controller;
  * Help api controller class.
  *
  * @package Modules\Help
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
